83de70d6c429836e82977059b5d0701006583fc6
APIVerve Team <hello@apiverve.com>
2024-08-20 11:10:15 -0500

    Bug Fixes

--- TenseConverterAPIClient.cs
+++ TenseConverterAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, tenseconverterQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, TenseConverterQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(tenseconverterQueryOptions options = null)
+        public ResponseObj Execute(TenseConverterQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(tenseconverterQueryOptions options)
+        private string constructURL(TenseConverterQueryOptions options)
         {
             string url = _apiEndpoint;
 
